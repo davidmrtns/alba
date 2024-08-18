9e0d93cebac9ef87b4aafa4ec8768c0af122bf53
davidmrtns <daviddmrtns@gmail.com>
2024-08-17 22:49:22 -0300

    Alterações estruturais

--- alba/__init__.py
+++ alba/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'c4a7705a9e3c2040aca1b9f803201fb6'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY_ALBA")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL_ALBA")
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
